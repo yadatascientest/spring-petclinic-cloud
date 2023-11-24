e0ad136f8b862ee22975302cba1f5e481ce4358d
DJOUDER <djouder@Ordinateur-portable-de-DJOUDER.local>
2023-11-24 20:50:53 +0100

    test 20

--- scripts/hellobash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-REPOSITORY_PREFIX=adjouder
-echo "début du déploeiement ......."
-echo ${REPOSITORY_PREFIX}"/spring-petclinic-cloud-api-gateway:latest"
-echo "Fin  du déploeiement ......."
