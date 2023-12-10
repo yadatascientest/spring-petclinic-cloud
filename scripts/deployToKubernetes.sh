e6e1ec075b34758599a74519826a2160e3c6c263
yadatascientest <133777990+yadatascientest@users.noreply.github.com>
2023-12-10 14:00:34 +0100

    Update deployToKubernetes.sh
    test

--- scripts/deployToKubernetes.sh
+++ scripts/deployToKubernetes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -z "${REPOSITORY_PREFIX}" ]
+if [ -z "${REPOSITORY_PREFIX_EL}" ]
 then 
-    echo "Please set the REPOSITORY_PREFIX"
+    echo "Please set the REPOSITORY_PREFIX_EL"
 else 
     cat ./k8s/*.yaml | \
-    sed 's#\${REPOSITORY_PREFIX}'"#${REPOSITORY_PREFIX}#g" | \
+    sed 's#\${REPOSITORY_PREFIX_EL}'"#${REPOSITORY_PREFIX_EL}#g" | \
     kubectl apply -f -
 fi
