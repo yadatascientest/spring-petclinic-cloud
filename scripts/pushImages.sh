0a87cdf8244c6a0744afbcdee54298165da6d078
yadatascientest <133777990+yadatascientest@users.noreply.github.com>
2023-12-20 21:30:26 +0100

    Update pushImages.sh
    test

--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 REPOSITORY_PREFIX_EL=elfiadylclc
 echo "début du déploeiement ......."
-echo ${REPOSITORY_PREFIX_EL}"/spring-petclinic-cloud-api-gateway:latest"
+echo "elfiadylclc/spring-petclinic-cloud-api-gateway:latest"
 
-docker push ${REPOSITORY_PREFIX_EL}/spring-petclinic-cloud-api-gateway:latest
-docker push ${REPOSITORY_PREFIX_EL}/spring-petclinic-cloud-visits-service:latest
-docker push ${REPOSITORY_PREFIX_EL}/spring-petclinic-cloud-vets-service:latest
-docker push ${REPOSITORY_PREFIX_EL}/spring-petclinic-cloud-customers-service:latest
-docker push ${REPOSITORY_PREFIX_EL}/spring-petclinic-cloud-admin-server:latest
-docker push ${REPOSITORY_PREFIX_EL}/spring-petclinic-cloud-discovery-service:latest
-docker push ${REPOSITORY_PREFIX_EL}/spring-petclinic-cloud-config-server:latest
+docker push elfiadylclc/spring-petclinic-cloud-api-gateway:latest
+docker push elfiadylclc/spring-petclinic-cloud-visits-service:latest
+docker push elfiadylclc/spring-petclinic-cloud-vets-service:latest
+docker push elfiadylclc/spring-petclinic-cloud-customers-service:latest
+docker push elfiadylclc/spring-petclinic-cloud-admin-server:latest
+docker push elfiadylclc/spring-petclinic-cloud-discovery-service:latest
+docker push elfiadylclc/spring-petclinic-cloud-config-server:latest
 
